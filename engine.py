251713b75e940a20f0a2a70bbe99ae2d4058d01d
daanniill <daniil.malakhov12@gmail.com>
2026-06-25 19:28:06 -0700

    Add children tracking to later build dependency graph

--- engine.py
+++ engine.py
@@ -1,7 +1,10 @@
 class Value:
 
-    def __init__(self, data):
+    # data: float, _children: tuple of two value objects, _op: operation performed on children
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self._prev = set(_children)
+        self._op = _op
 
     # Creates string representation of class
     def __repr__(self):
@@ -9,5 +12,10 @@ class Value:
 
     # defining addition of Value objects
     def __add__(self, other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+
+    # defining multiplication of Value objects
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '-')
         return out

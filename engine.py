dd67beed8f027d74a1535e976024dcafc11e2d8a
daanniill <daniil.malakhov12@gmail.com>
2026-06-25 19:15:43 -0700

    define addition of two Value objects

--- engine.py
+++ engine.py
@@ -6,3 +6,8 @@ class Value:
     # Creates string representation of class
     def __repr__(self):
         return f"Value(data={self.data})"
+
+    # defining addition of Value objects
+    def __add__(self, other):
+        out = Value(self.data + other.data)
+        return out

c133a145f5516600ccedee379fa2ccfaa8c21f33
daanniill <daniil.malakhov12@gmail.com>
2026-06-25 19:55:21 -0700

    added label variable to value class for graph visualization

--- engine.py
+++ engine.py
@@ -1,10 +1,11 @@
 class Value:
 
     # data: float, _children: tuple of two value objects, _op: operation performed on children
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.label = label
 
     # Creates string representation of class
     def __repr__(self):
@@ -17,5 +18,5 @@ class Value:
 
     # defining multiplication of Value objects
     def __mul__(self, other):
-        out = Value(self.data * other.data, (self, other), '-')
+        out = Value(self.data * other.data, (self, other), '*')
         return out

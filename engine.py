9dd7f6654d9503f6b36f3c6046ccdbef2224a730
daanniill <daniil.malakhov12@gmail.com>
2026-06-26 17:08:24 -0700

    implemented activation function tanh

--- engine.py
+++ engine.py
@@ -1,8 +1,12 @@
+import math
+
+
 class Value:
 
     # data: float, _children: tuple of two value objects, _op: operation performed on children
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
+        self.grad = 0.0
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -20,3 +24,14 @@ class Value:
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
         return out
+    
+    # implementing tanh as acitvation function
+    # reason why we are not breaking this down into smaller functions is because we don't need to.
+    # all we need to do is be able to find the local derivative, then from there we can construct a global derivative
+    # since we can differentiate tanh we can just implement it here, rather than implementing exp and divide
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+        return out
+    

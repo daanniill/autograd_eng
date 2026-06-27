32baf8aeaa9af705b9cece2547077be29c7df51a
daanniill <daniil.malakhov12@gmail.com>
2026-06-26 18:56:18 -0700

    added backward function to each operation in value class, avoids manual backpropogation and calculating of gradients

--- engine.py
+++ engine.py
@@ -1,12 +1,12 @@
 import math
 
-
 class Value:
 
     # data: float, _children: tuple of two value objects, _op: operation performed on children
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -18,13 +18,25 @@ class Value:
     # defining addition of Value objects
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
+        
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+        out._backward = _backward
+    
         return out
 
     # defining multiplication of Value objects
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
+        
+        def _backward():
+            self.grad = other.grad * out.grad
+            other.grad = self.data * out.grad
+        out._backward = _backward
+        
         return out
-    
+
     # implementing tanh as acitvation function
     # reason why we are not breaking this down into smaller functions is because we don't need to.
     # all we need to do is be able to find the local derivative, then from there we can construct a global derivative
@@ -33,5 +45,9 @@ class Value:
         x = self.data
         t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
         out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+        out._backward = _backward
+        
         return out
-    
